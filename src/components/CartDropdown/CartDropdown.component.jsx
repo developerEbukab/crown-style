e55ae03f278812d081437412febd8f99d49fc9c1
ChukwuEbuka Beluolisa <developerebukab@gmail.com>
2021-02-11 12:02:35 -0500

    added reselct for cat items

--- src/components/CartDropdown/CartDropdown.component.jsx
+++ src/components/CartDropdown/CartDropdown.component.jsx
@@ -3,9 +3,9 @@ import CustomButton from '../CustomButton/CustomButton.component';
 import "./CartDropdown.styles.scss";
 import {connect} from "react-redux"
 import CartItem from '../Cart-Item/Cart-Item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 const CartDropdown = ({ cartItems }) => {
-  console.log("CART ITEMS", cartItems[0])
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
@@ -18,8 +18,8 @@ const CartDropdown = ({ cartItems }) => {
   );
 }
 
-const mapStateToProps = ({cart: {cartItems} }) => ({
-  cartItems
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
